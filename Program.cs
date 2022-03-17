018fd70dc75a015c11ad900c93fc7e8bb22d9f56
henryhangit <henryhanying@qq.com>
2022-03-17 10:50:57 +0800

    another try

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.OpenApi.Models;
 using PizzaStore.Models;
 using Microsoft.EntityFrameworkCore;
 
-// this is in feature-devops-home-page branch mod2
+// this is in feature-devops-home-page branch
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
